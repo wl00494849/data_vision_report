dc090928009360f04a4ba8a4219d2d5fcca943b2
zhung1oz7 <wl00494849@gmail.com>
2023-10-13 17:35:52 +0800

    first commit

---
+++ num_cancer_trend.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import os 
+
+fig = plt.figure()
+ax = plt.axes()
+
+ax.set_title("Annual_Cencer")
+ax.set_xlabel('Annual')
+ax.set_ylabel('Death Population')
+annual_cencer_dt = pd.read_csv('annual_cencer.csv')
+
+x = annual_cencer_dt['年度'].array
+print(annual_cencer_dt.columns)
+ax.plot(x,annual_cencer_dt['氣管、支氣管和肺癌 '].values,label='氣管、支氣管和肺癌',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['肝和肝內膽管癌 '].values,label='Liver Cancer',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['結腸、直腸和肛門癌 '].values,label='Colorectal Cancer',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['女性乳癌 '].values,label='Breast Cancer',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['口腔癌 '].values,label='Oral Cancer',marker='o',markersize=4);
+plt.legend()
+plt.show();
+
+
+
