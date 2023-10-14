9c15ce58b5afd52d9605fa9d84fe7e8c44ad8342
zhung1oz7 <wl00494849@gmail.com>
2023-10-14 18:42:15 +0800

    update

--- num_cancer_trend.py
+++ num_cancer_trend.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
+import matplotlib
 import os 
 
 fig = plt.figure()
 ax = plt.axes()
 
-ax.set_title("National Annual Cancer Death Population")
-ax.set_xlabel('Annual')
-ax.set_ylabel('Death Population')
+matplotlib.rc('font', family='PingFang HK')
+plt.rcParams['font.sans-serif'] = 'PingFang HK'
+ax.set_title('全國前五癌症死亡人口趨勢')
+ax.set_xlabel('年度')
+ax.set_ylabel('死亡人口')
 annual_cencer_dt = pd.read_csv('annual_cencer.csv')
 
 # 取得年度欄位array
@@ -19,11 +22,11 @@ print(annual_cencer_dt.columns)
 # annual_cencer_dt['氣管、支氣管和肺癌 '].values 取得氣管、支氣管和肺癌內的值並轉換成陣列
 # marker 折線圖點
 # marker 折線圖點大小
-ax.plot(x,annual_cencer_dt['氣管、支氣管和肺癌'].values,label='Lung Cancer',marker='o',markersize=4);
-ax.plot(x,annual_cencer_dt['肝和肝內膽管癌'].values,label='Liver Cancer',marker='o',markersize=4);
-ax.plot(x,annual_cencer_dt['結腸、直腸和肛門癌'].values,label='Colorectal Cancer',marker='o',markersize=4);
-ax.plot(x,annual_cencer_dt['女性乳癌'].values,label='Breast Cancer',marker='o',markersize=4);
-ax.plot(x,annual_cencer_dt['口腔癌'].values,label='Oral Cancer',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['氣管、支氣管和肺癌'].values,label='氣管、支氣管和肺癌',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['肝和肝內膽管癌'].values,label='肝和肝內膽管癌',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['結腸、直腸和肛門癌'].values,label='結腸、直腸和肛門癌',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['女性乳癌'].values,label='女性乳癌',marker='o',markersize=4);
+ax.plot(x,annual_cencer_dt['口腔癌'].values,label='口腔癌',marker='o',markersize=4);
 
 # 畫出來
 plt.legend()
